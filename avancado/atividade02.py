cc95de6a75bd9dca2926115286f122946b207750
Pedro Batista Neto <pedroplpellegrinni1918@gmail.com>
2025-10-01 10:29:00 -0300

    seguindo com atividade02-avançado

--- avancado/atividade02.py
+++ avancado/atividade02.py
@@ -3,14 +3,20 @@ import time
 import pandas as pd
 
 url = 'https://rickandmortyapi.com/api/character/'
-while True:
+todos_os_dados = []
+while url:
     try:
         response = requests.get(url)
         response.raise_for_status()
         
         dados = response.json()
-        print(dados['info']['next'])
-        break
+        for p in range(len(dados['results'])):
+            todos_os_dados.append 
+            (dados['results'][p]['name'])
+
+        url = dados['info']['next']
+        
+        print(f"Página coletada. Próxima URL: {url}")
     except requests.exceptions.HTTPError as e:
         if e.response.status_code == 429:
             print('limite de requisição excedido, aguarde 10 segundos')
@@ -19,5 +25,7 @@ while True:
         else:
             print(f'Erro inesperado: {e}')
             break
+
+print(todos_os_dados)
 
 
